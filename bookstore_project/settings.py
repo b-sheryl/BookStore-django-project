535a20141a6f8b031946b28740b1db8eda08d5b6
sheryl <sheryl396@gmail.com>
2017-02-01 00:59:14 +0530

    Gmail SMTP settings

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -124,5 +124,15 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 #Registration
-ACCOUNT_ACTIVATE_DAYS = 7
+ACCOUNT_ACTIVATION_DAYS = 7
 REGISTRATION_AUTO_LOGIN = True
+LOGIN_REDIRECT_URL = '/store/'
+
+#Email settings
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = "sherylbernard10@gmail.com"
+EMAIL_PASSWORD = "123098abcd"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = "books@mysterybooks.com"
