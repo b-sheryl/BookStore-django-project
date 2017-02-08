e8a77e14ef0d4d71639a9e2435b2cb4b4fa08a0e
sheryl <sheryl396@gmail.com>
2017-02-08 14:23:58 +0530

    Renamed base template, included background image  in static files dir

--- store/views.py
+++ store/views.py
@@ -12,6 +12,6 @@ def store(request):
     request.session['location'] = "unknown"
     if request.user.is_authenticated():
         request.session['location'] = "Earth"
-    return render(request,'store.html',context)
+    return render(request,'base.html',context)
 
 
